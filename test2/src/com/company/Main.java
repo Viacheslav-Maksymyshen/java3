471ee453dcfaea9764d098e6716a7db81a986ed9
Mykola.Novytskyi <mnovytskyi@deloitte.com>
2019-03-11 16:52:49 +0200

    write to the file

--- test2/src/com/company/Main.java
+++ test2/src/com/company/Main.java
@@ -18,14 +18,17 @@ public class Main {
             String query = "call getReport();";
             Statement statement = connection.createStatement();
             ResultSet resSet = statement.executeQuery(query);
+            writer.write("Марка автомобиля\tГосударственный номер\tТип(легковой/грузовой)\tФИО водителя\tМарка топлива\t" +
+                    "Средний расход топлива\tПоказання одоментра на конец предыдущего месяца\tоказання одоментра на конец текущего месяца\n");
              while (resSet.next()){
-                 String mark;
-                 String number_column;
+                 String mark = resSet.getString("Mark");
+                 String number = resSet.getString("Number");
+                 String type = resSet.getString("type");
+                 String fuel = resSet.getString("Fuel");
+                 String name = resSet.getString("Name");
+                 String pricePerMonth = resSet.getString("PricePerMonth");
 
-                 mark = resSet.getString("Mark");
-                 number_column = resSet.getString("Number");
-
-                 writer.write(String.format("%s\t%s\n", mark, number_column));
+                 writer.write(String.format("%s\t%s\t%s\t%s\t%s\t%s\n", mark, number, type, fuel, name, pricePerMonth));
 
                  System.out.println();
              }
